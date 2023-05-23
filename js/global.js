1bee1ff54d18abb53aeee6d2721e90cd031f656f
drinb99 <134405812+drinb99@users.noreply.github.com>
2023-05-23 16:46:25 +0200

    Add files via upload

---
+++ js/global.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{delay: 200, origin: 'top'})
+sr.reveal('.form-container form',{delay: 800, origin: 'left'})
+sr.reveal('.heading',{delay: 800, origin: 'top'})
+sr.reveal('.vetura-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.sherbimet-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.rrethnesh-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.rishikimet-container',{delay: 600, origin: 'top'})
+sr.reveal('.informohu .box',{delay:  400, origin: 'bottom'})
+
+
+
+
